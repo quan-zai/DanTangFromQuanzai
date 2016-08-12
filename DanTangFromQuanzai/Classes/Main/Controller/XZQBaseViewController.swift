78f9f180d6b7a0b9036ca8d2f4689a89cda7291f
quanzai <1551205489@qq.com>
2016-08-12 23:29:40 +0800

    添加首页

--- DanTangFromQuanzai/Classes/Main/Controller/XZQBaseViewController.swift
+++ DanTangFromQuanzai/Classes/Main/Controller/XZQBaseViewController.swift
@@ -7,8 +7,22 @@
 //
 
 import UIKit
+import SVProgressHUD
+import FDFullscreenPopGesture
 
 class XZQBaseViewController: UIViewController
 {
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = XZQGlobalColor()
+        navigationController?.fd_prefersNavigationBarHidden = true
+        SVProgressHUD.setDefaultStyle(SVProgressHUDStyle.Custom)
+        SVProgressHUD.setMinimumDismissTimeInterval(1.0)
+        SVProgressHUD.setBackgroundColor(UIColor(red: 0, green: 0, blue: 0, alpha: 0.5))
+        SVProgressHUD.setForegroundColor(UIColor.whiteColor())
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
